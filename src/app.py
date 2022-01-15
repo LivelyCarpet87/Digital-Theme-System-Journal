ce1a4d8cc8e0e18f13a3dc5b99111029db5c0b47
Tyllis Xu <LivelyCarpet87@gmail.com>
2022-01-15 15:49:55 +0800

    New Draft Implementation

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFormLayout, QLineEdit, QGridLayout, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFormLayout, QLineEdit, QGridLayout, QCheckBox, QHBoxLayout
 
 class Boxes(QWidget):
 
@@ -14,18 +14,20 @@ class Boxes(QWidget):
             box.setCheckState(Qt.Unchecked)
             grid.addWidget(box, 0,i)
 
+
 class ThemeRow(QWidget):
 
     def __init__(self, inputText):
         super().__init__()
-        grid = QGridLayout()
-        self.setLayout(grid)
+        layout = QHBoxLayout()
+        self.setLayout(layout)
 
         self._themeName = QLineEdit(inputText)
-        grid.addWidget(self._themeName, 0,0)
+        self._themeName.setMaximumWidth(156)
+        layout.addWidget(self._themeName)
 
         self._boxes = Boxes()
-        grid.addWidget(self._boxes, 0,1)
+        layout.addWidget(self._boxes)
 
 
 
@@ -34,20 +36,34 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        themes = []
-
-        for i in range(7):
-            themes.append(ThemeRow(f"Item {i}"))
-
         self.setWindowTitle("Digital Theme System Journal")
-        flo = QFormLayout()
-        for itemNum in range(len(themes)):
-            flo.addRow(f"{itemNum+1}: ", themes[itemNum])
-        self.setLayout(flo)
+        self._flo = QFormLayout()
+        for itemNum in range(7):
+            self._flo.addRow("", ThemeRow(f"Item {itemNum}"))
+
+        self.setLayout(self._flo)
+
+    def addRow(self, location=-1):
+        if location == -1:
+            itemNum = self._flo.rowCount()
+            self._flo.addRow("", ThemeRow(f"Item {itemNum}"))
+        else:
+            self._flo.insertRow(location, "", ThemeRow(f"Item {location} Inserted"))
+
+    def rmRow(self, location=-1):
+        if location == -1:
+            itemNum = self._flo.rowCount() - 1
+            self._flo.removeRow(itemNum)
+        else:
+            self._flo.removeRow(location)
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
+window.addRow()
+window.addRow(location=4)
+window.rmRow()
+window.rmRow(location=5)
 
 app.exec()
