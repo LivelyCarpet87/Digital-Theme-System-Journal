bbafca4a8b35071ee1c301e493c30c11d13cfc25
Tyllis Xu <LivelyCarpet87@gmail.com>
2022-01-15 16:20:30 +0800

    Draft 3 (Scrapped)

--- src/app.py
+++ src/app.py
@@ -1,61 +1,65 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QFormLayout, QLineEdit, QGridLayout, QCheckBox, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QGridLayout, QCheckBox
 
-class Boxes(QWidget):
+class Box(QCheckBox):
 
     def __init__(self):
         super().__init__()
-        grid = QGridLayout()
-        self.setLayout(grid)
-
-        for i in range(7):
-            box = QCheckBox()
-            box.setCheckState(Qt.Unchecked)
-            grid.addWidget(box, 0,i)
+        self.setCheckState(Qt.Unchecked)
 
 
-class ThemeRow(QWidget):
+class ThemeName(QLineEdit):
 
     def __init__(self, inputText):
-        super().__init__()
-        layout = QHBoxLayout()
-        self.setLayout(layout)
+        super().__init__(inputText)
+        self.setMaximumWidth(156)
 
-        self._themeName = QLineEdit(inputText)
-        self._themeName.setMaximumWidth(156)
-        layout.addWidget(self._themeName)
+class ColNameTop(QLineEdit):
 
-        self._boxes = Boxes()
-        layout.addWidget(self._boxes)
+    def __init__(self, inputText):
+        super().__init__(inputText)
+        self.setMaximumWidth(64)
 
+class ColNameBottom(QLineEdit):
 
+    def __init__(self, inputText):
+        super().__init__(inputText)
+        self.setMaximumWidth(64)
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("Digital Theme System Journal")
-        self._flo = QFormLayout()
-        for itemNum in range(7):
-            self._flo.addRow("", ThemeRow(f"Item {itemNum}"))
+        self._layout = QGridLayout()
 
-        self.setLayout(self._flo)
+        for i in range(1,8):
+            self._layout.addWidget(ColNameTop(""), 0, i)
+
+        for i in range(1,8):
+            self._layout.addWidget(ColNameBottom(""), 1, i)
+
+        for i in range(2,9):
+            self.addRow()
+
+        self.setLayout(self._layout)
+
 
     def addRow(self, location=-1):
         if location == -1:
-            itemNum = self._flo.rowCount()
-            self._flo.addRow("", ThemeRow(f"Item {itemNum}"))
+            row = self._layout.rowCount()
+            self._layout.addWidget(ThemeName(""), row, 0)
+            for i in range(1,8):
+                self._layout.addWidget(Box(), row, i, Qt.AlignHCenter)
         else:
-            self._flo.insertRow(location, "", ThemeRow(f"Item {location} Inserted"))
+            row = location
 
     def rmRow(self, location=-1):
         if location == -1:
-            itemNum = self._flo.rowCount() - 1
-            self._flo.removeRow(itemNum)
+            row = self._layout.rowCount() - 1
         else:
-            self._flo.removeRow(location)
+            pass
 
 app = QApplication(sys.argv)
 
